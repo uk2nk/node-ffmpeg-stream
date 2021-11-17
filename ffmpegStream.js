c51f791dc2bf408a437b341fe47c0218cbe0ffb3
Kannan <kannaa.krishnaa@gmail.com>
2021-11-17 09:05:02 +0530

    Stream, Socket Open Close detail added.
    Stream, Socket Open Close detail added.

--- ffmpegStream.js
+++ ffmpegStream.js
@@ -10,19 +10,20 @@ class Stream extends EventEmitter {
     super();
 	this.name=input.name;	
     this.start(input);
-    
+    this.wsCount=0;
   }
   start(input) {
     this.startStream(input);
   }
   setOptions(input) {	  	 
-    const options = {
-      "-rtsp_transport": "tcp",
-      "-i": input.url,
+    const options = {		
+      "-rtsp_transport": "tcp", 
+	"-i": input.url,	
+"-pix_fmt":"yuv420p",	
       "-f": "mpegts",
       "-codec:v": "mpeg1video",
-      "-codec:a": "mp2",           
-      "-r": input.options["-r"]?input.options["-r"]:30,
+      "-codec:a": "mp2",  	    
+      "-r": input.options["-r"]?input.options["-r"]:30,	 
     };
     let params = [];
     for (let key in options) {
@@ -42,26 +43,62 @@ class Stream extends EventEmitter {
 	  }
     }
   }
-  params.push(...this.additionalFlags);  
+  params.push(...this.additionalFlags); 
+
     params.push("-");
+	console.log(params)
     return params;
   }
-  startStream(input) {
-    this.wss = new WebSocket.Server({ port: input.wsPort });		
-    this.child = child_process.spawn("ffmpeg", this.setOptions(input));		 
-    this.child.stdout.on("data", (data) => {		
-      this.wss.clients.forEach((client) => {		 
+  startStream(input) {	
+var inputData=[];
+var gettingInputData,gettingOutputData;  
+
+    this.wss = new WebSocket.Server({ port: input.wsPort }); 	
+    this.child = child_process.spawn("ffmpeg", this.setOptions(input),{
+    detached: false
+  });
+   console.log(this.name+ " Stream Started...");
+    console.log("Waiting for socket connection(s)...");
+    this.child.stdout.on("data", (data) => {	
+	this.wss.clients.forEach((client) => {		 
         client.send(data);
-      }); 	    
+      }); 
       return this.emit("data", data);
     });
-	this.wss.on("connection", (socket, request) => {
-    return this.onSocketConnect(socket, request)
+	
+	this.child.stderr.on("data", (data) => {	
+		 data = data.toString();			 
+		if (data.indexOf('Input #') !== -1) { 		
+	   gettingInputData=true;	  
+    }	
+	if (data.indexOf('Output #') !== -1) {
+      gettingInputData = false	 
+      gettingOutputData = true
+	  console.log(data);
+    }
+	 if(gettingInputData){	
+  	 console.log(data);
+	 this.wss.clients.forEach((client) => {	
+		console.log("Inside Clients");	 
+        client.send(data);
+      }); 
+      return this.emit("data", data);
+	 }	
+		
+	});
+ this.wss.on("connection", (socket, request) => {
+    this.wsCount=this.wss.clients.size;
+	this.onSocketConnect(socket,request);
+  })	  
+ this.wss.on("close", (socket, request) => {
+    console.log(this.name+ " : Stream Stopped.");
   })
+  
   }
   
   onSocketConnect = function(socket, request) {  
-  console.log(this.name +` :: New socket connected [ Total connection(s) : ` + this.wss.clients.size+"]" );  
+  
+  console.log(this.name +` :: New socket connected [ Total connection(s) : ` + this.wsCount+"]" );    
   socket.remoteAddress = request.connection.remoteAddress
   return socket.on("close", (code, message) => {	 
     return console.log(this.name +` :: Socket disconnected [ Remaining connection(s) : ` + this.wss.clients.size+"]" );  
